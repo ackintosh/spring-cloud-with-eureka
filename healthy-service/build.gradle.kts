6267897374d3724476b3041c326200f9cbcffa27
ackintosh <sora.akatsuki@gmail.com>
2020-01-14 20:07:10 +0900

    Bump up JVM version to 11

--- healthy-service/build.gradle.kts
+++ healthy-service/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 group = "io.github.ackintosh"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_1_8
+java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
     mavenCentral()
@@ -40,6 +40,6 @@ tasks.withType<Test> {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
 }
